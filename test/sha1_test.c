4ccac25fbe8fffe8c9f14c4154929bac60a55dba
Baptiste <baptiste.le-glaunec@epitech.eu>
2022-08-29 11:38:54 +0200

    more tests for sha1

--- test/sha1_test.c
+++ test/sha1_test.c
@@ -10,7 +10,7 @@
   do {                                              \
     s_str *hash;                                    \
     s_str stra;                                     \
-    str_init_1(&stra, false, "abc");                \
+    str_init_1(&stra, false, test);                \
     hash = sha1(&stra);                             \
     TEST_STRNCMP(hash->ptr.p, result, hash->size);  \
     str_delete(hash);                               \
@@ -19,6 +19,8 @@
 void sha1_test_sha1 ()
 {
   SHA1_TEST_SHA1("abc", "\xa9\x99\x3e\x36\x47\x06\x81\x6a\xba\x3e\x25\x71\x78\x50\xc2\x6c\x9c\xd0\xd8\x9d");
+  SHA1_TEST_SHA1("", "\xda\x39\xa3\xee\x5e\x6b\x4b\x0d\x32\x55\xbf\xef\x95\x60\x18\x90\xaf\xd8\x07\x09");
+  SHA1_TEST_SHA1("abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq", "\x84\x98\x3e\x44\x1c\x3b\xd2\x6e\xba\xae\x4a\xa1\xf9\x51\x29\xe5\xe5\x46\x70\xf1");
 }
 
 void sha1_test ()
