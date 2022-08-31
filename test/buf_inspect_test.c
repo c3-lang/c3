fbfd60b8ddf8f3fca8598649c74f836aee02693a
Thomas de Grivel <thoxdg@gmail.com>
2022-08-31 16:14:38 +0200

    buf_inspect_test_

--- test/buf_inspect_test.c
+++ test/buf_inspect_test.c
@@ -7,7 +7,7 @@
 #include "../libc3/str.h"
 #include "test.h"
 
-#define BUF_TEST_INSPECT_CHARACTER(test, result)                       \
+#define BUF_INSPECT_TEST_CHARACTER(test, result)                       \
   do {                                                                 \
     test_context("buf_inspect_character(" # test ") -> " # result);    \
     buf_init(&buf, false, sizeof(b), b);                               \
@@ -15,7 +15,7 @@
     TEST_STRNCMP(buf.ptr.ps8, result, buf.wpos);                       \
   } while (0)
 
-#define BUF_TEST_INSPECT_STR_CHARACTER(test, result)                   \
+#define BUF_INSPECT_TEST_STR_CHARACTER(test, result)                   \
   do {                                                                 \
     test_context("buf_inspect_str_character(" # test ") -> "           \
                  # result);                                            \
@@ -24,7 +24,7 @@
     TEST_STRNCMP(buf.ptr.ps8, result, buf.wpos);                       \
   } while (0)
 
-#define BUF_TEST_INSPECT_STR_CHARACTER_SIZE(test, result)              \
+#define BUF_INSPECT_TEST_STR_CHARACTER_SIZE(test, result)              \
   do {                                                                 \
     test_context("buf_inspect_str_character_size("                     \
                  # test ") -> " # result);                             \
@@ -32,70 +32,70 @@
     TEST_EQ(buf_inspect_str_character_size(test), result);             \
   } while (0)
 
-void buf_test_inspect_character ();
-void buf_test_inspect_str_character ();
-void buf_test_inspect_str_character_size ();
+void buf_inspect_test_character ();
+void buf_inspect_test_str_character ();
+void buf_inspect_test_str_character_size ();
 
 void buf_inspect_test ()
 {
-  buf_test_inspect_str_character_size();
-  buf_test_inspect_str_character();
-  buf_test_inspect_character();
+  buf_inspect_test_str_character_size();
+  buf_inspect_test_str_character();
+  buf_inspect_test_character();
 }
 
-void buf_test_inspect_character ()
+void buf_inspect_test_character ()
 {
   s8 b[32];
   s_buf buf;
-  BUF_TEST_INSPECT_CHARACTER(0, "'\\0'");
-  BUF_TEST_INSPECT_CHARACTER(1, "'\\x01'");
-  BUF_TEST_INSPECT_CHARACTER(2, "'\\x02'");
-  BUF_TEST_INSPECT_CHARACTER('0', "'0'");
-  BUF_TEST_INSPECT_CHARACTER('9', "'9'");
-  BUF_TEST_INSPECT_CHARACTER('A', "'A'");
-  BUF_TEST_INSPECT_CHARACTER('Z', "'Z'");
-  BUF_TEST_INSPECT_CHARACTER('a', "'a'");
-  BUF_TEST_INSPECT_CHARACTER('z', "'z'");
-  BUF_TEST_INSPECT_CHARACTER(928, "'Π'");
-  BUF_TEST_INSPECT_CHARACTER(0xFF, "'ÿ'");
-  BUF_TEST_INSPECT_CHARACTER(42164, "'꒴'");
-  BUF_TEST_INSPECT_CHARACTER(65856, "'𐅀'");
+  BUF_INSPECT_TEST_CHARACTER(0, "'\\0'");
+  BUF_INSPECT_TEST_CHARACTER(1, "'\\x01'");
+  BUF_INSPECT_TEST_CHARACTER(2, "'\\x02'");
+  BUF_INSPECT_TEST_CHARACTER('0', "'0'");
+  BUF_INSPECT_TEST_CHARACTER('9', "'9'");
+  BUF_INSPECT_TEST_CHARACTER('A', "'A'");
+  BUF_INSPECT_TEST_CHARACTER('Z', "'Z'");
+  BUF_INSPECT_TEST_CHARACTER('a', "'a'");
+  BUF_INSPECT_TEST_CHARACTER('z', "'z'");
+  BUF_INSPECT_TEST_CHARACTER(928, "'Π'");
+  BUF_INSPECT_TEST_CHARACTER(0xFF, "'ÿ'");
+  BUF_INSPECT_TEST_CHARACTER(42164, "'꒴'");
+  BUF_INSPECT_TEST_CHARACTER(65856, "'𐅀'");
 }
 
-void buf_test_inspect_str_character ()
+void buf_inspect_test_str_character ()
 {
   s8 b[32];
   s_buf buf;
-  BUF_TEST_INSPECT_STR_CHARACTER(0, "\\0");
-  BUF_TEST_INSPECT_STR_CHARACTER(1, "\\x01");
-  BUF_TEST_INSPECT_STR_CHARACTER(2, "\\x02");
-  BUF_TEST_INSPECT_STR_CHARACTER('0', "0");
-  BUF_TEST_INSPECT_STR_CHARACTER('9', "9");
-  BUF_TEST_INSPECT_STR_CHARACTER('A', "A");
-  BUF_TEST_INSPECT_STR_CHARACTER('Z', "Z");
-  BUF_TEST_INSPECT_STR_CHARACTER('a', "a");
-  BUF_TEST_INSPECT_STR_CHARACTER('z', "z");
-  BUF_TEST_INSPECT_STR_CHARACTER(928, "Π");
-  BUF_TEST_INSPECT_STR_CHARACTER(0xFF, "ÿ");
-  BUF_TEST_INSPECT_STR_CHARACTER(42164, "꒴");
-  BUF_TEST_INSPECT_STR_CHARACTER(65856, "𐅀");
+  BUF_INSPECT_TEST_STR_CHARACTER(0, "\\0");
+  BUF_INSPECT_TEST_STR_CHARACTER(1, "\\x01");
+  BUF_INSPECT_TEST_STR_CHARACTER(2, "\\x02");
+  BUF_INSPECT_TEST_STR_CHARACTER('0', "0");
+  BUF_INSPECT_TEST_STR_CHARACTER('9', "9");
+  BUF_INSPECT_TEST_STR_CHARACTER('A', "A");
+  BUF_INSPECT_TEST_STR_CHARACTER('Z', "Z");
+  BUF_INSPECT_TEST_STR_CHARACTER('a', "a");
+  BUF_INSPECT_TEST_STR_CHARACTER('z', "z");
+  BUF_INSPECT_TEST_STR_CHARACTER(928, "Π");
+  BUF_INSPECT_TEST_STR_CHARACTER(0xFF, "ÿ");
+  BUF_INSPECT_TEST_STR_CHARACTER(42164, "꒴");
+  BUF_INSPECT_TEST_STR_CHARACTER(65856, "𐅀");
 }
 
-void buf_test_inspect_str_character_size ()
+void buf_inspect_test_str_character_size ()
 {
   s8 b[32];
   s_buf buf;
-  BUF_TEST_INSPECT_STR_CHARACTER_SIZE(0, 2);
-  BUF_TEST_INSPECT_STR_CHARACTER_SIZE(1, 4);
-  BUF_TEST_INSPECT_STR_CHARACTER_SIZE(2, 4);
-  BUF_TEST_INSPECT_STR_CHARACTER_SIZE('0', 1);
-  BUF_TEST_INSPECT_STR_CHARACTER_SIZE('9', 1);
-  BUF_TEST_INSPECT_STR_CHARACTER_SIZE('A', 1);
-  BUF_TEST_INSPECT_STR_CHARACTER_SIZE('Z', 1);
-  BUF_TEST_INSPECT_STR_CHARACTER_SIZE('a', 1);
-  BUF_TEST_INSPECT_STR_CHARACTER_SIZE('z', 1);
-  BUF_TEST_INSPECT_STR_CHARACTER_SIZE(928, 2);
-  BUF_TEST_INSPECT_STR_CHARACTER_SIZE(0xFF, 2);
-  BUF_TEST_INSPECT_STR_CHARACTER_SIZE(42164, 3);
-  BUF_TEST_INSPECT_STR_CHARACTER_SIZE(65856, 4);
+  BUF_INSPECT_TEST_STR_CHARACTER_SIZE(0, 2);
+  BUF_INSPECT_TEST_STR_CHARACTER_SIZE(1, 4);
+  BUF_INSPECT_TEST_STR_CHARACTER_SIZE(2, 4);
+  BUF_INSPECT_TEST_STR_CHARACTER_SIZE('0', 1);
+  BUF_INSPECT_TEST_STR_CHARACTER_SIZE('9', 1);
+  BUF_INSPECT_TEST_STR_CHARACTER_SIZE('A', 1);
+  BUF_INSPECT_TEST_STR_CHARACTER_SIZE('Z', 1);
+  BUF_INSPECT_TEST_STR_CHARACTER_SIZE('a', 1);
+  BUF_INSPECT_TEST_STR_CHARACTER_SIZE('z', 1);
+  BUF_INSPECT_TEST_STR_CHARACTER_SIZE(928, 2);
+  BUF_INSPECT_TEST_STR_CHARACTER_SIZE(0xFF, 2);
+  BUF_INSPECT_TEST_STR_CHARACTER_SIZE(42164, 3);
+  BUF_INSPECT_TEST_STR_CHARACTER_SIZE(65856, 4);
 }
