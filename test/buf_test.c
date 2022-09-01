f4f124b1ca00283687312f25acb4e27e47704195
Baptiste <baptiste.le-glaunec@epitech.eu>
2022-09-01 16:53:31 +0200

    add buf_test_read_s8

--- test/buf_test.c
+++ test/buf_test.c
@@ -60,6 +60,7 @@ void buf_test_init_clean ();
 void buf_test_new_delete ();
 void buf_test_new_alloc_delete ();
 void buf_test_peek ();
+void buf_test_read_s8 ();
 void buf_test_read_u8 ();
 void buf_test_read_character ();
 void buf_test_write_u8 ();
@@ -71,6 +72,7 @@ void buf_test ()
   buf_test_new_delete();
   buf_test_new_alloc_delete();
   buf_test_peek();
+  buf_test_read_s8();
   buf_test_read_u8();
   buf_test_read_character();
   buf_test_write_u8();
@@ -159,6 +161,49 @@ void buf_test_new_alloc_delete ()
 
 void buf_test_peek ()
 {
+}
+
+void buf_test_read_s8 ()
+{
+  char a[9] = "ABCDEFGH";
+  s_buf buf;
+  s8 byte;
+  buf_init(&buf, false, sizeof(a), a);
+  TEST_EQ(buf_read_s8(&buf, &byte), 0);
+  TEST_EQ(buf_read_s8(&buf, &byte), 0);
+  buf.wpos = 1;
+  TEST_EQ(buf_read_s8(&buf, &byte), 1);
+  TEST_EQ(byte, 'A');
+  TEST_EQ(buf.rpos, 1);
+  TEST_EQ(buf_read_s8(&buf, &byte), 0);
+  TEST_EQ(buf_read_s8(&buf, &byte), 0);
+  buf.wpos = 5;
+  TEST_EQ(buf_read_s8(&buf, &byte), 1);
+  TEST_EQ(byte, 'B');
+  TEST_EQ(buf.rpos, 2);
+  TEST_EQ(buf_read_s8(&buf, &byte), 1);
+  TEST_EQ(byte, 'C');
+  TEST_EQ(buf.rpos, 3);
+  TEST_EQ(buf_read_s8(&buf, &byte), 1);
+  TEST_EQ(byte, 'D');
+  TEST_EQ(buf.rpos, 4);
+  TEST_EQ(buf_read_s8(&buf, &byte), 1);
+  TEST_EQ(byte, 'E');
+  TEST_EQ(buf.rpos, 5);
+  TEST_EQ(buf_read_s8(&buf, &byte), 0);
+  TEST_EQ(buf_read_s8(&buf, &byte), 0);
+  buf.wpos = 8;
+  TEST_EQ(buf_read_s8(&buf, &byte), 1);
+  TEST_EQ(byte, 'F');
+  TEST_EQ(buf.rpos, 6);
+  TEST_EQ(buf_read_s8(&buf, &byte), 1);
+  TEST_EQ(byte, 'G');
+  TEST_EQ(buf.rpos, 7);
+  TEST_EQ(buf_read_s8(&buf, &byte), 1);
+  TEST_EQ(byte, 'H');
+  TEST_EQ(buf.rpos, 8);
+  TEST_EQ(buf_read_s8(&buf, &byte), 0);
+  buf_clean(&buf);
 }
 
 void buf_test_read_u8 ()
