f79516a8e7778d96034a95a3450f52f61515b5ae
Thomas de Grivel <thoxdg@gmail.com>
2022-09-01 15:12:30 +0200

    add test section names

--- test/c3_test.c
+++ test/c3_test.c
@@ -14,20 +14,34 @@ void sha1_test ();
 int main (int argc, char **argv)
 {
   test_init(argc, argv);
-  if (test_target("types"))
+  if (test_target("types")) {
+    printf("\ntypes\n");
     types_test();
-  if (test_target("character"))
+  }
+  if (test_target("character")) {
+    printf("\ncharacter\n");
     character_test();
-  if (test_target("str"))
+  }
+  if (test_target("str")) {
+    printf("\nstr\n");
     str_test();
-  if (test_target("sym"))
+  }
+  if (test_target("sym")) {
+    printf("\nsym\n");
     sym_test();
-  if (test_target("buf"))
+  }
+  if (test_target("buf")) {
+    printf("\nbuf\n");
     buf_test();
-  if (test_target("buf_inspect"))
+  }
+  if (test_target("buf_inspect")) {
+    printf("\nbuf_inspect\n");
     buf_inspect_test();
-  if (test_target("sha1"))
+  }
+  if (test_target("sha1")) {
+    printf("\nsha1\n");
     sha1_test();
+  }
   test_summary();
   test_shutdown();
   return 0;
