074deb73a305a621afee584e9ec25c2d0b18f9bb
Thomas de Grivel <thoxdg@gmail.com>
2022-09-02 20:05:16 +0200

    character_test

--- test/character_test.c
+++ test/character_test.c
@@ -6,48 +6,48 @@
 #include "../libc3/character.h"
 #include "test.h"
 
-void character_test_character_1 ();
-void character_test_character_is_lowercase ();
-void character_test_character_is_uppercase ();
-void character_test_character_utf8 ();
-void character_test_character_utf8_size ();
+void character_test_1 ();
+void character_test_is_lowercase ();
+void character_test_is_uppercase ();
+void character_test_utf8 ();
+void character_test_utf8_size ();
 
 void character_test ()
 {
-  character_test_character_1();
-  character_test_character_is_lowercase();
-  character_test_character_is_uppercase();
-  character_test_character_utf8_size();
-  character_test_character_utf8();
+  character_test_1();
+  character_test_is_lowercase();
+  character_test_is_uppercase();
+  character_test_utf8_size();
+  character_test_utf8();
 }
 
-#define CHARACTER_TEST_CHARACTER_1_ASCII(string)        \
+#define CHARACTER_TEST_1_ASCII(string)        \
   TEST_EQ(character_1(string), string[0])
 
-void character_test_character_1 ()
+void character_test_1 ()
 {
-  CHARACTER_TEST_CHARACTER_1_ASCII("0");
-  CHARACTER_TEST_CHARACTER_1_ASCII("1");
-  CHARACTER_TEST_CHARACTER_1_ASCII("2");
-  CHARACTER_TEST_CHARACTER_1_ASCII("3");
-  CHARACTER_TEST_CHARACTER_1_ASCII("4");
-  CHARACTER_TEST_CHARACTER_1_ASCII("5");
-  CHARACTER_TEST_CHARACTER_1_ASCII("6");
-  CHARACTER_TEST_CHARACTER_1_ASCII("7");
-  CHARACTER_TEST_CHARACTER_1_ASCII("8");
-  CHARACTER_TEST_CHARACTER_1_ASCII("9");
-  CHARACTER_TEST_CHARACTER_1_ASCII("A");
-  CHARACTER_TEST_CHARACTER_1_ASCII("B");
-  CHARACTER_TEST_CHARACTER_1_ASCII("C");
-  CHARACTER_TEST_CHARACTER_1_ASCII("X");
-  CHARACTER_TEST_CHARACTER_1_ASCII("Y");
-  CHARACTER_TEST_CHARACTER_1_ASCII("Z");
-  CHARACTER_TEST_CHARACTER_1_ASCII("a");
-  CHARACTER_TEST_CHARACTER_1_ASCII("b");
-  CHARACTER_TEST_CHARACTER_1_ASCII("c");
-  CHARACTER_TEST_CHARACTER_1_ASCII("x");
-  CHARACTER_TEST_CHARACTER_1_ASCII("y");
-  CHARACTER_TEST_CHARACTER_1_ASCII("z");
+  CHARACTER_TEST_1_ASCII("0");
+  CHARACTER_TEST_1_ASCII("1");
+  CHARACTER_TEST_1_ASCII("2");
+  CHARACTER_TEST_1_ASCII("3");
+  CHARACTER_TEST_1_ASCII("4");
+  CHARACTER_TEST_1_ASCII("5");
+  CHARACTER_TEST_1_ASCII("6");
+  CHARACTER_TEST_1_ASCII("7");
+  CHARACTER_TEST_1_ASCII("8");
+  CHARACTER_TEST_1_ASCII("9");
+  CHARACTER_TEST_1_ASCII("A");
+  CHARACTER_TEST_1_ASCII("B");
+  CHARACTER_TEST_1_ASCII("C");
+  CHARACTER_TEST_1_ASCII("X");
+  CHARACTER_TEST_1_ASCII("Y");
+  CHARACTER_TEST_1_ASCII("Z");
+  CHARACTER_TEST_1_ASCII("a");
+  CHARACTER_TEST_1_ASCII("b");
+  CHARACTER_TEST_1_ASCII("c");
+  CHARACTER_TEST_1_ASCII("x");
+  CHARACTER_TEST_1_ASCII("y");
+  CHARACTER_TEST_1_ASCII("z");
   TEST_EQ(character_1("À"), 192);
   TEST_EQ(character_1("É"), 201);
   TEST_EQ(character_1("à"), 224);
@@ -59,13 +59,16 @@ void character_test_character_1 ()
   TEST_EQ(character_1("ꝝ"), 42845);
 }
 
-void character_test_character_is_lowercase ()
+void character_test_is_lowercase ()
 {
   character c;
   for (c = -10; c < 40; c++)
     TEST_ASSERT(! character_is_lowercase(c));
-  TEST_ASSERT(! character_is_lowercase('0'));
-  TEST_ASSERT(! character_is_lowercase('0'));
+  TEST_ASSERT(! character_is_lowercase('\x00'));
+  TEST_ASSERT(! character_is_lowercase('\x01'));
+  TEST_ASSERT(! character_is_lowercase('\r'));
+  TEST_ASSERT(! character_is_lowercase('\n'));
+  TEST_ASSERT(! character_is_lowercase(' '));
   TEST_ASSERT(! character_is_lowercase('0'));
   TEST_ASSERT(! character_is_lowercase('1'));
   TEST_ASSERT(! character_is_lowercase('8'));
@@ -94,7 +97,7 @@ void character_test_character_is_lowercase ()
   TEST_ASSERT(character_is_lowercase(character_1("ꝝ")));
 }
 
-void character_test_character_is_uppercase ()
+void character_test_is_uppercase ()
 {
   character c;
   for (c = -10; c < 40; c++)
@@ -127,11 +130,11 @@ void character_test_character_is_uppercase ()
   TEST_ASSERT(character_is_uppercase(character_1("Ꝝ")));
 }
 
-void character_test_character_utf8 ()
+void character_test_utf8 ()
 {
 }
 
-void character_test_character_utf8_size ()
+void character_test_utf8_size ()
 {
   character c;
   for (c = -10; c < 0; c++)
