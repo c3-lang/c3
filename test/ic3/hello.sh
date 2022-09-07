d77123f391c09ef73baabf9ebab5200e022353d1
Thomas de Grivel <thoxdg@gmail.com>
2022-09-07 17:27:42 +0200

    ic3_test

--- test/ic3/hello.sh
+++ test/ic3/hello.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-{
-    echo "\"Hello, world !\"" |
-        $IC3 > hello.out 2> hello.err
-} &&
-    diff -u hello.out.expected hello.out &&
-    diff -u hello.err.expected hello.err
+$IC3 < hello.in > hello.out 2> hello.err
+echo $? > hello.ret
+diff -u hello.out.expected hello.out &&
+    diff -u hello.err.expected hello.err &&
+    diff -u hello.ret.expected hello.ret
