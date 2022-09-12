02cceeae4f951db095684f16dfa0ce6b50205478
Thomas de Grivel <thoxdg@gmail.com>
2022-09-12 21:49:08 +0200

    integer

--- libc3/integer.c
+++ libc3/integer.c
@@ -6,226 +6,237 @@
 #include <stdlib.h>
 #include "integer.h"
 
-s_integer * integer_init (s_integer *i)
+s_integer * integer_abs (const s_integer *a, s_integer *dest)
 {
-  int r;
-  assert(i);
-  if ((r = mp_init(&i->mp_int)) != MP_OKAY)
-    err(1, "integer_init: %s", mp_error_to_string(r));
-  return i;
+  sw r;
+  assert(a);
+  assert(dest);
+  if ((r = mp_abs(&a->mp_int, &dest->mp_int)) != MP_OKAY)
+    errx(1, "integer_abs: %s", mp_error_to_string(r));
+  return dest;
 }
 
-void integer_clean(s_integer *i)
+s_integer * integer_add (const s_integer *a, const s_integer *b,
+                         s_integer *dest)
 {
-  assert(i);
-  mp_clear(&i->mp_int);
+  sw r;
+  assert(dest);
+  assert(a);
+  assert(b);
+  if ((r = mp_add(&a->mp_int, &b->mp_int, &dest->mp_int)) != MP_OKAY)
+    errx(1, "integer_add: %s", mp_error_to_string(r));
+  return dest;
 }
 
-s_integer * integer_copy (s_integer *i, s_integer *j)
+s_integer * integer_band (const s_integer *a, const s_integer *b,
+                          s_integer *dest)
 {
-  int r;
-  assert(i);
-  assert(j);
-  if ((r = mp_copy(&i->mp_int, &j->mp_int)) != MP_OKAY)
-    err(1, "integer_copy: %s", mp_error_to_string(r));
-  return j;
+  sw r;
+  assert(a);
+  assert(b);
+  assert(dest);
+  if ((r = mp_and(&a->mp_int, &b->mp_int, &dest->mp_int)) != MP_OKAY)
+    errx(1, "integer_band: %s", mp_error_to_string(r));
+  return dest;
 }
 
-s_integer * integer_new_copy (s_integer *i)
+s_integer * integer_bnot (const s_integer *a, s_integer *dest)
 {
-  s_integer *j;
-  assert(i);
-  j = malloc(sizeof(s_integer));
-  if (!j)
-    err(1, "integer_new_copy: malloc");
-  return integer_copy(i, j);
+  sw r;
+  assert(a);
+  assert(dest);
+  if ((r = mp_complement(&a->mp_int, &dest->mp_int)) != MP_OKAY)
+    errx(1, "integer_bnot: %s", mp_error_to_string(r));
+  return dest;
 }
 
-int integer_cmp (s_integer *i, s_integer *j)
+s_integer * integer_bor (const s_integer *a, const s_integer *b,
+                         s_integer *dest)
 {
-  int r;
-  assert(i);
-  assert(j);
-  r = mp_cmp(&i->mp_int, &j->mp_int);
+  sw r;
+  assert(a);
+  assert(b);
+  assert(dest);
+  if ((r = mp_or(&a->mp_int, &b->mp_int, &dest->mp_int)) != MP_OKAY)
+    errx(1, "integer_bor: %s", mp_error_to_string(r));
+  return dest;
+}
+
+s_integer * integer_bxor (const s_integer *a, const s_integer *b,
+                          s_integer *dest)
+{
+  sw r;
+  assert(a);
+  assert(b);
+  assert(dest);
+  if ((r = mp_xor(&a->mp_int, &b->mp_int, &dest->mp_int)) != MP_OKAY)
+    errx(1, "integer_bxor: %s", mp_error_to_string(r));
+  return dest;
+}
+
+void integer_clean (s_integer *dest)
+{
+  assert(dest);
+  mp_clear(&dest->mp_int);
+}
+
+sw integer_cmp (const s_integer *a, const s_integer *b)
+{
+  sw r;
+  assert(a);
+  assert(b);
+  r = mp_cmp(&a->mp_int, &b->mp_int);
   if (r == MP_GT)
     return 1;
   if (r == MP_EQ)
     return 0;
+  if (r == MP_LT)
+    return -1;
+  errx(1, "integer_cmp: %s", mp_error_to_string(r));
   return -1;
 }
 
-s_integer * integer_add (s_integer *i, s_integer *j, s_integer *k)
+s_integer * integer_copy (const s_integer *a, s_integer *dest)
 {
-  int r;
-  assert(i);
-  assert(j);
-  assert(k);
-  if ((r = mp_add(&i->mp_int, &j->mp_int, &k->mp_int)) != MP_OKAY)
-    err(1, "integer_add: %s", mp_error_to_string(r));
-  return k;
+  sw r;
+  assert(a);
+  assert(dest);
+  if ((r = mp_copy(&a->mp_int, &dest->mp_int)) != MP_OKAY)
+    errx(1, "integer_copy: %s", mp_error_to_string(r));
+  return dest;
 }
 
-s_integer * integer_sub (s_integer *i, s_integer *j, s_integer *k)
+s_integer * integer_div (const s_integer *a, const s_integer *b,
+                         s_integer *dest)
 {
-  int r;
-  assert(i);
-  assert(j);
-  assert(k);
-  if ((r = mp_sub(&i->mp_int, &j->mp_int, &k->mp_int)) != MP_OKAY)
-    err(1, "integer_sub: %s", mp_error_to_string(r));
-  return k;
+  sw r;
+  assert(a);
+  assert(b);
+  assert(dest);
+  if ((r = mp_div(&a->mp_int, &b->mp_int, &dest->mp_int,
+                  NULL)) != MP_OKAY)
+    errx(1, "integer_div: %s", mp_error_to_string(r));
+  return dest;
 }
 
-s_integer * integer_mul (s_integer *i, s_integer *j, s_integer *k)
+s_integer * integer_gcd (const s_integer *a, const s_integer *b,
+                         s_integer *dest)
 {
-  int r;
-  assert(i);
+  sw r;
+  assert(dest);
   assert(j);
   assert(k);
-  if ((r = mp_mul(&i->mp_int, &j->mp_int, &k->mp_int)) != MP_OKAY)
-    err(1, "integer_mul: %s", mp_error_to_string(r));
-  return k;
+  if ((r = mp_gcd(&a->mp_int, &b->mp_int, &dest->mp_int)) != MP_OKAY)
+    errx(1, "integer_gcd: %s", mp_error_to_string(r));
+  return dest;
 }
 
-s_integer * integer_div (s_integer *i, s_integer *j, s_integer *k)
+s_integer * integer_init (s_integer *dest)
 {
-  int r;
-  assert(i);
-  assert(j);
-  assert(k);
-  if ((r = mp_div(&i->mp_int, &j->mp_int, &k->mp_int, NULL)) != MP_OKAY)
-    err(1, "integer_div: %s", mp_error_to_string(r));
-  return k;
+  sw r;
+  assert(dest);
+  if ((r = mp_init(&dest->mp_int)) != MP_OKAY)
+    errx(1, "integer_init: %s", mp_error_to_string(r));
+  return dest;
 }
 
-s_integer * integer_mod (s_integer *i, s_integer *j, s_integer *k)
+s_integer * integer_lcm (const s_integer *a, const s_integer *b,
+                         s_integer *dest)
 {
-  int r;
-  assert(i);
-  assert(j);
-  assert(k);
-  if ((r = mp_mod(&i->mp_int, &j->mp_int, &k->mp_int)) != MP_OKAY)
-    err(1, "integer_mod: %s", mp_error_to_string(r));
-  return k;
+  sw r;
+  assert(a);
+  assert(b);
+  assert(dest);
+  if ((r = mp_lcm(&a->mp_int, &b->mp_int, &dest->mp_int)) != MP_OKAY)
+    errx(1, "integer_lcm: %s", mp_error_to_string(r));
+  return dest;
 }
 
-s_integer * integer_pow (s_integer *i, s_integer *j, s_integer *k)
+s_integer * integer_lshift (s_integer *a, sw b, s_integer *dest)
 {
-  int r;
-  assert(i);
-  assert(j);
+  sw r;
+  assert(dest);
   assert(k);
-  if ((r = mp_exptmod(&i->mp_int, &j->mp_int, &k->mp_int, &k->mp_int))
+  if ((r = mp_mul_2d(&a->mp_int, b, &dest->mp_int)) != MP_OKAY)
+    errx(1, "integer_lshift: %s", mp_error_to_string(r));
+  return dest;
+}
+
+s_integer * integer_mod (const s_integer *a, const s_integer *b,
+                         s_integer *dest)
+{
+  sw r;
+  assert(a);
+  assert(b);
+  assert(dest);
+  if ((r = mp_mod(&a->mp_int, &b->mp_int, &dest->mp_int)) != MP_OKAY)
+    errx(1, "integer_mod: %s", mp_error_to_string(r));
+  return dest;
+}
+
+s_integer * integer_mul (const s_integer *a, const s_integer *b,
+                         s_integer *dest)
+{
+  sw r;
+  assert(a);
+  assert(b);
+  assert(dest);
+  if ((r = mp_mul(&a->mp_int, &b->mp_int, &dest->mp_int)) != MP_OKAY)
+    errx(1, "integer_mul: %s", mp_error_to_string(r));
+  return dest;
+}
+
+s_integer * integer_neg (const s_integer *a, s_integer *dest)
+{
+  sw r;
+  assert(a);
+  assert(dest);
+  if ((r = mp_neg(&a->mp_int, &dest->mp_int)) != MP_OKAY)
+    errx(1, "integer_neg: %s", mp_error_to_string(r));
+  return dest;
+}
+
+s_integer * integer_new_copy (const s_integer *a)
+{
+  s_integer *dest;
+  assert(a);
+  dest = malloc(sizeof(s_integer));
+  if (!dest)
+    errx(1, "integer_new_copy: out of memory");
+  return integer_copy(a, dest);
+}
+
+s_integer * integer_pow (const s_integer *a, const s_integer *b,
+                         s_integer *dest)
+{
+  sw r;
+  assert(a);
+  assert(b);
+  assert(dest);
+  if ((r = mp_exptmod(&a->mp_int, &b->mp_int, &dest->mp_int, &dest->mp_int))
       != MP_OKAY)
-    err(1, "integer_pow: %s", mp_error_to_string(r));
-  return k;
+    errx(1, "integer_pow: %s", mp_error_to_string(r));
+  return dest;
 }
 
-s_integer * integer_neg (s_integer *i, s_integer *j)
+s_integer * integer_sqrt (const s_integer *a, s_integer *dest)
 {
-  int r;
-  assert(i);
-  assert(j);
-  if ((r = mp_neg(&i->mp_int, &j->mp_int)) != MP_OKAY)
-    err(1, "integer_neg: %s", mp_error_to_string(r));
-  return j;
+  sw r;
+  assert(a);
+  assert(dest);
+  if ((r = mp_sqrt(&a->mp_int, &dest->mp_int)) != MP_OKAY)
+    errx(1, "integer_sqrt: %s", mp_error_to_string(r));
+  return dest;
 }
 
-s_integer * integer_abs (s_integer *i, s_integer *j)
+s_integer * integer_sub (const s_integer *a, const s_integer *b,
+                         s_integer *dest)
 {
-  int r;
-  assert(i);
-  assert(j);
-  if ((r = mp_abs(&i->mp_int, &j->mp_int)) != MP_OKAY)
-    err(1, "integer_abs: %s", mp_error_to_string(r));
-  return j;
-}
-
-s_integer * integer_lshift (s_integer *i, int j, s_integer *k)
-{
-  int r;
-  assert(i);
-  assert(k);
-  if ((r = mp_mul_2d(&i->mp_int, j, &k->mp_int)) != MP_OKAY)
-    err(1, "integer_lshift: %s", mp_error_to_string(r));
-  return k;
-}
-
-//Algebraic operations
-
-s_integer * integer_gcd (s_integer *i, s_integer *j, s_integer *k)
-{
-  int r;
-  assert(i);
-  assert(j);
-  assert(k);
-  if ((r = mp_gcd(&i->mp_int, &j->mp_int, &k->mp_int)) != MP_OKAY)
-    err(1, "integer_gcd: %s", mp_error_to_string(r));
-  return k;
-}
-
-s_integer * integer_lcm (s_integer *i, s_integer *j, s_integer *k)
-{
-  int r;
-  assert(i);
-  assert(j);
-  assert(k);
-  if ((r = mp_lcm(&i->mp_int, &j->mp_int, &k->mp_int)) != MP_OKAY)
-    err(1, "integer_lcm: %s", mp_error_to_string(r));
-  return k;
-}
-
-s_integer * integer_sqrt (s_integer *i, s_integer *j)
-{
-  int r;
-  assert(i);
-  assert(j);
-  if ((r = mp_sqrt(&i->mp_int, &j->mp_int)) != MP_OKAY)
-    err(1, "integer_sqrt: %s", mp_error_to_string(r));
-  return j;
-}
-
-//Bitwise operations
-
-s_integer * integer_and (s_integer *i, s_integer *j, s_integer *k)
-{
-  int r;
-  assert(i);
-  assert(j);
-  assert(k);
-  if ((r = mp_and(&i->mp_int, &j->mp_int, &k->mp_int)) != MP_OKAY)
-    err(1, "integer_and: %s", mp_error_to_string(r));
-  return k;
-}
-
-s_integer * integer_or (s_integer *i, s_integer *j, s_integer *k)
-{
-  int r;
-  assert(i);
-  assert(j);
-  assert(k);
-  if ((r = mp_or(&i->mp_int, &j->mp_int, &k->mp_int)) != MP_OKAY)
-    err(1, "integer_or: %s", mp_error_to_string(r));
-  return k;
-}
-
-s_integer * integer_xor (s_integer *i, s_integer *j, s_integer *k)
-{
-  int r;
-  assert(i);
-  assert(j);
-  assert(k);
-  if ((r = mp_xor(&i->mp_int, &j->mp_int, &k->mp_int)) != MP_OKAY)
-    err(1, "integer_xor: %s", mp_error_to_string(r));
-  return k;
-}
-
-s_integer * integer_not (s_integer *i, s_integer *j)
-{
-  int r;
-  assert(i);
-  assert(j);
-  if ((r = mp_complement(&i->mp_int, &j->mp_int)) != MP_OKAY)
-    err(1, "integer_not: %s", mp_error_to_string(r));
-  return j;
+  sw r;
+  assert(dest);
+  assert(a);
+  assert(b);
+  if ((r = mp_sub(&a->mp_int, &b->mp_int, &dest->mp_int)) != MP_OKAY)
+    errx(1, "integer_sub: %s", mp_error_to_string(r));
+  return dest;
 }
--- libc3/integer.h
+++ libc3/integer.h
@@ -18,13 +18,43 @@
 
 /* Stack allocation compatible functions */
 s_integer * integer_init (s_integer *i);
-void integer_clean (s_integer *i);
+void        integer_clean (s_integer *i);
+
+/* Modifiers */
+s_integer * integer_abs (const s_integer *a, s_integer *dest);
+s_integer * integer_add (const s_integer *a, const s_integer *b,
+                         s_integer *dest);
+s_integer * integer_band (const s_integer *a, const s_integer *b,
+                          s_integer *dest);
+s_integer * integer_bnot (const s_integer *a, s_integer *dest);
+s_integer * integer_bor (const s_integer *a, const s_integer *b,
+                         s_integer *dest);
+s_integer * integer_bxor (const s_integer *a, const s_integer *b,
+                          s_integer *dest);
+s_integer * integer_copy (const s_integer *a, s_integer *dest);
+s_integer * integer_div (const s_integer *a, const s_integer *b,
+                         s_integer *dest);
+s_integer * integer_gcd (const s_integer *a, const s_integer *b,
+                         s_integer *dest);
+s_integer * integer_lcm (const s_integer *a, const s_integer *b,
+                         s_integer *dest);
+s_integer * integer_lshift (s_integer *a, sw b, s_integer *dest);
+s_integer * integer_mod (const s_integer *a, const s_integer *b,
+                         s_integer *dest);
+s_integer * integer_mul (const s_integer *a, const s_integer *b,
+                         s_integer *dest);
+s_integer * integer_neg (const s_integer *a, s_integer *dest);
+s_integer * integer_pow (const s_integer *a, const s_integer *b,
+                         s_integer *dest);
+s_integer * integer_sqrt (const s_integer *a, s_integer *dest);
+s_integer * integer_sub (const s_integer *a, const s_integer *b,
+                         s_integer *dest);
 
 /* Constructors, call integer_delete after use. */
 s_integer * integer_new ();
+s_integer * integer_new_copy (const s_integer *a);
 
-s_integer integer_set_base (s_integer *i, int j);
-char * mp_to_string(s_integer *i);
-int integer_cmp (s_integer *i, s_integer *j);
+/* Observers */
+sw integer_cmp (const s_integer *a, const s_integer *b);
 
 #endif /* INTEGER_H */
