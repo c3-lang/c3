6ef25365734b1993a4ea32e511be17f1a2282eed
Thomas de Grivel <thoxdg@gmail.com>
2022-09-14 14:51:56 +0200

    wip buf_inspect_test_list

--- test/buf_inspect_test.c
+++ test/buf_inspect_test.c
@@ -5,69 +5,90 @@
 #include <string.h>
 #include "../libc3/buf.h"
 #include "../libc3/buf_inspect.h"
+#include "../libc3/buf_parse.h"
 #include "../libc3/str.h"
 #include "test.h"
 
-#define BUF_INSPECT_TEST_BOOL(test, result)                            \
+#define BUF_INSPECT_TEST_BOOL(test, expected)                          \
   do {                                                                 \
     s_buf buf;                                                         \
-    test_context("buf_inspect_bool(" # test ") -> " # result);         \
+    test_context("buf_inspect_bool(" # test ") -> " # expected);       \
     BUF_INIT_ALLOCA(&buf, 16);                                         \
-    TEST_EQ(buf_inspect_bool(&buf, (test)), strlen(result));           \
-    TEST_STRNCMP(buf.ptr.p, (result), buf.wpos);                       \
+    TEST_EQ(buf_inspect_bool(&buf, (test)), strlen(expected));         \
+    TEST_STRNCMP(buf.ptr.p, (expected), buf.wpos);                     \
     test_context(NULL);                                                \
   } while (0)
   
-#define BUF_INSPECT_TEST_CHARACTER(test, result)                       \
+#define BUF_INSPECT_TEST_CHARACTER(test, expected)                     \
   do {                                                                 \
     s8 b[32];                                                          \
     s_buf buf;                                                         \
-    test_context("buf_inspect_character(" # test ") -> " # result);    \
+    test_context("buf_inspect_character(" # test ") -> " # expected);  \
     buf_init(&buf, false, sizeof(b), b);                               \
-    TEST_EQ(buf_inspect_character(&buf, test), strlen(result));        \
-    TEST_STRNCMP(buf.ptr.ps8, result, buf.wpos);                       \
+    TEST_EQ(buf_inspect_character(&buf, test), strlen(expected));      \
+    TEST_STRNCMP(buf.ptr.ps8, expected, buf.wpos);                     \
     test_context(NULL);                                                \
   } while (0)
 
-#define BUF_INSPECT_TEST_STR_CHARACTER(test, result)                   \
+#define BUF_INSPECT_TEST_LIST(test, expected)                          \
+  do {                                                                 \
+    s_buf buf;                                                         \
+    s_buf buf_test;                                                    \
+    s_list *list_test;                                                 \
+    test_context("buf_inspect_list(" # test ") -> " # expected);       \
+    buf_init_1(&buf_test, (test));                                     \
+    if (buf_parse_list(&buf_test, &list_test) != strlen(test))         \
+      errx(1, "buf_parse_list");                                       \
+    buf_init_alloc(&buf, 1024 * 1024);                                 \
+    TEST_EQ(buf_inspect_list(&buf, list_test), strlen(expected));      \
+    TEST_EQ(buf.wpos, strlen(expected));                               \
+    if (g_test_last_ok)                                                \
+      TEST_STRNCMP(buf.ptr.p, (expected), buf.wpos);                   \
+    buf_clean(&buf_test);                                              \
+    buf_clean(&buf);                                                   \
+    test_context(NULL);                                                \
+  } while (0)
+
+#define BUF_INSPECT_TEST_STR_CHARACTER(test, expected)                 \
   do {                                                                 \
     s8 b[32];                                                          \
     s_buf buf;                                                         \
     test_context("buf_inspect_str_character(" # test ") -> "           \
-                 # result);                                            \
+                 # expected);                                          \
     buf_init(&buf, false, sizeof(b), b);                               \
-    TEST_EQ(buf_inspect_str_character(&buf, test), strlen(result));    \
-    TEST_STRNCMP(buf.ptr.ps8, result, buf.wpos);                       \
+    TEST_EQ(buf_inspect_str_character(&buf, test), strlen(expected));  \
+    TEST_STRNCMP(buf.ptr.ps8, expected, buf.wpos);                     \
     test_context(NULL);                                                \
   } while (0)
 
-#define BUF_INSPECT_TEST_STR_CHARACTER_SIZE(test, result)              \
+#define BUF_INSPECT_TEST_STR_CHARACTER_SIZE(test, expected)            \
   do {                                                                 \
     s8 b[32];                                                          \
     s_buf buf;                                                         \
     test_context("buf_inspect_str_character_size("                     \
-                 # test ") -> " # result);                             \
+                 # test ") -> " # expected);                           \
     buf_init(&buf, false, sizeof(b), b);                               \
-    TEST_EQ(buf_inspect_str_character_size(test), result);             \
+    TEST_EQ(buf_inspect_str_character_size(test), expected);           \
     test_context(NULL);                                                \
   } while (0)
 
-#define BUF_INSPECT_TEST_STR(test, result)                             \
+#define BUF_INSPECT_TEST_STR(test, expected)                           \
   do {                                                                 \
     s8 b[1024];                                                        \
     s_buf buf;                                                         \
     s_str str;                                                         \
-    test_context("buf_inspect_str(" # test ") -> " # result);          \
+    test_context("buf_inspect_str(" # test ") -> " # expected);        \
     str_init_1(&str, false, (test));                                   \
     buf_init(&buf, false, sizeof(b), b);                               \
-    TEST_EQ(buf_inspect_str(&buf, &str), strlen(result));              \
-    TEST_STRNCMP(buf.ptr.p, (result), buf.wpos);                       \
+    TEST_EQ(buf_inspect_str(&buf, &str), strlen(expected));            \
+    TEST_STRNCMP(buf.ptr.p, (expected), buf.wpos);                     \
     test_context(NULL);                                                \
   } while (0)
 
 void buf_inspect_test ();
 void buf_inspect_test_bool ();
 void buf_inspect_test_character ();
+void buf_inspect_test_list ();
 void buf_inspect_test_str_character ();
 void buf_inspect_test_str_character_size ();
 void buf_inspect_test_str ();
@@ -78,6 +99,7 @@ void buf_inspect_test ()
   buf_inspect_test_str_character_size();
   buf_inspect_test_str_character();
   buf_inspect_test_character();
+  buf_inspect_test_list();
   buf_inspect_test_str();
 }
 
@@ -106,6 +128,11 @@ void buf_inspect_test_character ()
   BUF_INSPECT_TEST_CHARACTER(128516, "'😄'");
   BUF_INSPECT_TEST_CHARACTER(128995, "'🟣'");
   BUF_INSPECT_TEST_CHARACTER(129321, "'🤩'");
+}
+
+void buf_inspect_test_list ()
+{
+  BUF_INSPECT_TEST_LIST("[]", "[]");
 }
 
 void buf_inspect_test_str_character ()
